620f99714d4efa9ccbb6dc3614888c3d63e2df52
SevlaMare <sevilamare@gmail.com>
2020-05-11 16:57:56 -0300

    linter fix

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
 
-  validates :username, presence: true, uniqueness: true, length: {in: 4..12}
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {in: 6..16}
+  validates :password, presence: true, length: { in: 6..16 }
 end
